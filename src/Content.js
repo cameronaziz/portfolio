d7bda5c6c0d4aba3d396e1c9f572075332938fb0
cameronaziz <cameronaziz@me.com>
2019-07-30 16:15:31 -0400

    move

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,6 @@
 import React, { Component, createRef } from 'react';
 import { Route, Switch, withRouter } from 'react-static';
+import { Helmet } from 'react-helmet';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import * as Containers from './containers';
@@ -104,6 +105,26 @@ class Content extends Component {
         onKeyDown={this.handleOutsideClick}
         id="wrapper"
       >
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Cameron Aziz - Javascript Engineer</title>
+          <link rel="apple-touch-icon" sizes="57x57" href="/images/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/images/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/images/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/images/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/images/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/images/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/images/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/images/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192"  href="/images/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+          <link rel="manifest" href="/images/manifest.json" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        </Helmet>
         <Header introTitle={introTitle} openArticle={this.toggleArticle} articleOpen={articleOpen} />
         <div id="main" style={mainStyle}>
           <Switch>
